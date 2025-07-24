a070e8a13a84c15db98b308ed889f468c0bc1032
Ivandro Macheque <machequeivandro444@gmail.com>
2025-07-24 11:16:39 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,79 @@
+# 🏡 Previsão de Preços de Imóveis
+
+Este é um projeto de Machine Learning que realiza a análise, o treinamento e a implantação de um modelo de regressão para prever o preço de imóveis. O projeto inclui um Jupyter Notebook com todo o processo de análise e modelagem e uma aplicação web interativa criada com Streamlit para realizar previsões em tempo real.
+
+# ✨ Funcionalidades
+
+Análise Exploratória de Dados (EDA): Investigação detalhada do conjunto de dados para identificar correlações, distribuições e outliers.
+
+Treinamento de Modelo: Utilização de um modelo de Regressão Linear Regularizada (ElasticNet) para aprender os padrões dos dados.
+
+Aplicação Web Interativa: Uma interface simples onde o usuário pode inserir as características de um imóvel e receber uma estimativa de preço instantaneamente.
+
+Histórico de Previsões: A aplicação web armazena e exibe um histórico das previsões realizadas durante a sessão.
+
+Exportação de Dados: Permite baixar o histórico de previsões como um ficheiro CSV.
+
+# Escolha do modelo
+
+O modelo de Regressão Linear foi escolhido devido a linearidade da coluna 'Square_Footage' com a coluna alvo 'House_Price', com uma correlação
+de 99%. Devido aos outliers, foi criada uma coluna categórica para separá-los com ranges ('Square_Footage_Category'), o baixo e o alto foram colocados para os outliers, e o médio para os valore normais aumentando assim a eficiência do modelo.
+
+# 💻 Tecnologias Utilizadas
+
+Este projeto foi construído utilizando as seguintes tecnologias:
+
+### Linguagem: Python 3
+
+### Análise e Modelagem:
+
+ - Jupyter Notebook
+
+ - Pandas
+
+ - NumPy
+
+ - Scikit-learn
+
+### Visualização de Dados:
+
+ - Matplotlib
+
+ - Seaborn
+
+### Aplicação Web:
+
+ - Streamlit
+
+### Serialização do Modelo:
+
+ - Pickle
+
+# 📁 Estrutura do Projeto
+.
+├── notebook/
+│   └── main.ipynb                     # Notebook com a análise e treinamento do modelo
+├── app.py                             # Script da aplicação Streamlit
+├── house_price_regression_dataset.csv # Dataset original
+├── Linear Regression.pkl              # Modelo de regressão treinado e salvo
+├── README.md                          # Este ficheiro
+└── requirements.txt                   # Dependências do projeto
+
+# 📈 Uso da Aplicação
+
+### Modo de Previsão (Price Predict):
+
+1. Use os seletores e campos de entrada para inserir as características do imóvel (área, ano de construção, etc.).
+
+2. Clique no botão "Realizar Previsão".
+
+3. O preço estimado será exibido na tela.
+
+### Modo de Histórico (Predicted Data):
+
+1. Acesse esta opção na barra lateral para visualizar uma tabela com todas as previsões feitas na sessão atual.
+
+2. Utilize os botões para limpar o histórico ou baixá-lo como um ficheiro .csv.
+
+# 🔗 Link da Aplicação
+
---
+++ app.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+with open("Linear Regression.pkl", "rb") as f:
+    modelo = pickle.load(f)
+
+if "historic" not in st.session_state:
+    st.session_state.historic = pd.DataFrame()
+
+with st.sidebar:
+    st.header('Options')
+    decision = st.selectbox("Mode", ['Price Predict', 'Predicted Data'])
+    st.markdown("### 🔗 Links Úteis")
+    st.markdown("[👨‍💻 Meu GitHub](https://github.com/IvandroMacheque/IvandroMacheque.git)")
+    st.markdown("[💼 Meu LinkedIn](https://www.linkedin.com/in/ivandromacheque?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app)")
+
+if decision == 'Price Predict':
+
+    st.title("🏡 House Price Prediction")
+    st.write('## Insira os Dados da Casa')
+
+    col1, col2 = st.columns(2)
+    with col2:
+        lot_size = st.slider('Lot Size', 0.500000, 5.000000, 2.000000)
+    with col1:
+        square_meters = st.slider('Área da Casa (m²)', 30.0, 500.0, 245.0)
+        square_footage = square_meters * 10.7639104
+
+    col6, col7 = st.columns(2)
+    with col6:
+        Neighborhood_Quality = st.slider('Qualidade dos Vizinhos', 1, 5, 10)
+    with col7:
+        year_built = st.number_input("Ano de Construção")
+
+    col3, col4, col5 = st.columns(3)
+    with col3:
+        num_bedrooms = st.radio(
+                "Número de Quartos",
+                options=[1, 2, 3, 4, 5, 6],
+            )
+    with col4:
+        num_bathrooms = st.radio(
+                "Número de Banheiros",
+                options=[1, 2, 3, 4],
+            )
+    with col5:
+        garage_size = st.radio(
+                "Tamanho da Garagem",
+                options=["Sem Garagem", 1, 2, 3],
+            )
+        if garage_size == "Sem Garagem":
+            garage_size = 0
+
+    data_modelo = {'Square_Footage': square_footage,
+            'Year_Built': year_built,
+            'Lot_Size': lot_size}
+    dados_modelo = pd.DataFrame(data_modelo, index=[0])
+    bins = [0 , 1600, 3999, np.inf]
+    labels = ['baixo', 'medio', 'alto']
+    dados_modelo['Square_Footage_Category'] = pd.cut(dados_modelo['Square_Footage'], bins=bins, labels=labels)
+    dados_modelo['Square_Footage_Category'] = dados_modelo['Square_Footage_Category'].map({'baixo': 0,
+                                                                            'medio': 1,
+                                                                            'alto': 2})
+
+    def prever():
+        previsao = modelo.predict(dados_modelo)
+        with container:
+            st.success(f"### O Preço previsto da casa é de {previsao[0]:.2f}")
+        data = {'Square_Footage': square_footage,
+            'Num_Bedrooms': num_bedrooms,
+            'Num_Bathrooms': num_bathrooms,
+            'Year_Built': year_built,
+            'Lot_Size': lot_size,
+            'Garage_Size': garage_size,
+            'Neighborhood_Quality': Neighborhood_Quality,
+            'House_Price': previsao[0]}
+        dados = pd.DataFrame(data, index=[0])
+        st.session_state.historic = pd.concat([st.session_state.historic, dados], ignore_index=True)
+
+    container = st.container()
+    st.button("Realizar Previsão", on_click=prever)
+
+else:
+    st.title("📜 Forecast History")
+
+    st.dataframe(st.session_state.historic)
+
+    # linha de botoes
+    but1, but2 = st.columns(2)
+    # funcao para limpar o historico
+    with but1:
+        if not st.session_state.historic.empty:
+            def limpar():
+                st.session_state.historic = pd.DataFrame()
+            st.button("Limpar histórico", on_click=limpar)
+
+    # funcao para salvar o historico
+    with but2:
+        if not st.session_state.historic.empty:
+            def convert_for_download(df):
+                df = pd.DataFrame()
+                return df.to_csv(index=False).encode("utf-8")
+            
+            file = convert_for_download(st.session_state.historic)
+            st.download_button(
+                label="Baixar Histórico",
+                data=file,
+                file_name='Precos Previstos.csv',
+                mime='text/csv'
+            )
